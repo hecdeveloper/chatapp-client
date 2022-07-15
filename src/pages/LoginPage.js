7f1fe848d21d61b61a562e60e6b6ade79df08c10
Hector Ibarra <hector@oneseventech.com>
2022-07-15 12:53:35 -0400

    adding styles

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -5,7 +5,7 @@ export const LoginPage = () => {
     return (
         <form className="login100-form validate-form flex-sb flex-w">
             <span className="login100-form-title mb-3">
-                Chat - Ingreso
+                Chat - Login
             </span>
             
             <div className="wrap-input100 validate-input mb-3">
@@ -29,7 +29,7 @@ export const LoginPage = () => {
 
                 <div className="col text-right">
                     <Link to="/auth/register" className="txt1">
-                        Nueva cuenta?
+                       New Account
                     </Link>
                 </div>
             </div>
